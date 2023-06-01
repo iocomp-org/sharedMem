773e56f2fb47fc4a8d9e0826c8110574147d4215
Shrey <sb15895@my.bristol.ac.uk>
2023-06-01 11:58:27 +0100

    updates

--- oneArray.c
+++ oneArray.c
@@ -48,6 +48,7 @@ int main(int argc, char** argv)
 	 */  
 	colour = globalRank%(globalSize/2); // IO rank and comp rank have same colour
 	MPI_Comm_split(MPI_COMM_WORLD, colour, globalRank, &newComm); 
+	// newComm = MPI_COMM_WORLD; 
 	// test
 	int newRank; 
 	MPI_Comm_rank(newComm,&newRank); 
@@ -55,56 +56,49 @@ int main(int argc, char** argv)
 	MPI_Win win; 
 
 
-//	// timers 
-//	double start, stop, diff; 
-//
-//	start = MPI_Wtime();
-//
-//	if(newRank == 0) // comp process 
-//	{
-//		printf("memory initialised by newRank %i \n", newRank); 
-//		ierr = MPI_Win_create(array, soi*N, soi, MPI_INFO_NULL, newComm, &win);
-//		error_check(ierr); 
-//	} 
-//
-//	else
-//	{
-//		// non 0 rank creates window using NULL pointer 
-//		ierr = MPI_Win_create(NULL, 0,1, MPI_INFO_NULL, newComm, &win);
-//		error_check(ierr); 
-//	} 
-//	
-//	if(newRank == 0)
-//	{
-//		initialise(array, 5); 
-//	}
-//	else
-//	{
-//		initialise(array, 7); 
-//	}
-//	// start window 
-//	MPI_Win_fence(MPI_MODE_NOPRECEDE,win);
-//	if(newRank != 0)
-//	{
-//		MPI_Get(array, N, MPI_INT, 0,0,N, MPI_INT, win); 
-//	} 
-//	MPI_Win_fence(MPI_MODE_NOSUCCEED, win);
-//
-//	MPI_Win_free(&win);
-//	// end window
-//	
-//	// print values 
-//	if(newRank != 0)
-//	{
-//		printf("Global rank %i reading array with value set to be 5 \n",globalRank); 
-//		printData(array); 
-//	} 
-//
-//	stop = MPI_Wtime();
-//	diff = stop - start;
-//
-//	printf("Rank %d, took %8.8fs\n",
-//			globalRank,  diff);
+	// start timer 
+	double start, stop, diff; 
+	start = MPI_Wtime();
+
+	// comp process initialises array and creates a window with that array 
+	if(newRank == 0)  
+	{
+		initialise(array, 5); 
+		printf("memory initialised by newRank %i \n", newRank); 
+		ierr = MPI_Win_create(array, soi*N, soi, MPI_INFO_NULL, newComm, &win);
+		error_check(ierr); 
+	} 
+
+	else
+	{
+		// non 0 rank creates window using NULL pointer 
+		ierr = MPI_Win_create(NULL, 0,1, MPI_INFO_NULL, newComm, &win);
+		error_check(ierr); 
+	} 
+	
+	// start window 
+	MPI_Win_fence(MPI_MODE_NOPRECEDE,win);
+	if(newRank != 0)
+	{
+		MPI_Get(array, N, MPI_INT, 0,0,N, MPI_INT, win); 
+	} 
+	MPI_Win_fence(MPI_MODE_NOSUCCEED, win);
+
+	MPI_Win_free(&win);
+	// end window
+	
+	// print values 
+	if(newRank != 0)
+	{
+		printf("Global rank %i reading array with value set to be 5 \n",globalRank); 
+		printData(array); 
+	} 
+
+	stop = MPI_Wtime();
+	diff = stop - start;
+
+	printf("Rank %d, took %8.8fs\n",
+			globalRank,  diff);
 
 	MPI_Finalize();
 	return 0;
