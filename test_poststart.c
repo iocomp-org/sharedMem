3f9095e0dcb0b1502f7669cee560598eaee735ac
Shrey <sb15895@my.bristol.ac.uk>
2023-06-01 14:03:04 +0100

    epoch access updates to get value from rank 0

--- test_poststart.c
+++ test_poststart.c
@@ -8,29 +8,34 @@ int main(int argc, char** argv)
 	MPI_Init(&argc, &argv);
 	MPI_Status status; 
 	int ierr, i; 
-	int ranks[3];  
 	MPI_Win win;
 
-	int rank; 
+	int rank, size;  
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+	MPI_Comm_size(MPI_COMM_WORLD, &size); 
+	int ranks[size];  
 
 	//Start up MPI...
 	MPI_Group comm_group, group;
 
-	for (i=0;i<3;i++) {
+	for (i=0;i<size;i++) {
 		ranks[i] = i;     //For forming groups, later
 	}
 	MPI_Comm_group(MPI_COMM_WORLD,&comm_group);
 
-	// allocate memory
+	// allocate memory and initialise buf array 
 	int soi = sizeof(int);
 	int* buf = (int *)malloc(soi*N); 
-	buf[0]=rank; 
 
 	/* Create new window for this comm */
 	if (rank == 0) {
-		MPI_Win_create(buf,sizeof(int)*3,sizeof(int),
+		MPI_Win_create(buf,sizeof(int)*N,sizeof(int),
 				MPI_INFO_NULL,MPI_COMM_WORLD,&win);
+		// initialise array 
+		for(i = 0; i < N; i++)
+		{
+			buf[i] = 5; 
+		}
 	}
 	else {
 		/* Rank 1 or 2 */
@@ -40,7 +45,7 @@ int main(int argc, char** argv)
 	/* Now do the communication epochs */
 	if (rank == 0) {
 		/* Origin group consists of ranks 1 and 2 */
-		MPI_Group_incl(comm_group,2,ranks+1,&group);
+		MPI_Group_incl(comm_group,size-1,ranks+1,&group);
 		/* Begin the exposure epoch */
 		MPI_Win_post(group,0,win);
 		/* Wait for epoch to end */
@@ -51,25 +56,27 @@ int main(int argc, char** argv)
 		MPI_Group_incl(comm_group,1,ranks,&group);
 		/* Begin the access epoch */
 		MPI_Win_start(group,0,win);
-		/* Put into rank==0 according to my rank */
-		MPI_Put(buf,1,MPI_INT,0,rank,1,MPI_INT,win);
+		/* Get from rank==0 according to my rank */
+		MPI_Get(buf,N,MPI_INT,0,0,N,MPI_INT,win);
 		/* Terminate the access epoch */
 		MPI_Win_complete(win);
 	}
 	
-	if(rank == 0)
+	/* Free window and groups */
+	MPI_Win_free(&win);
+	MPI_Group_free(&group);
+	MPI_Group_free(&comm_group);
+	
+	/* Print values and check */ 
+	if(rank != 0)
 	{
-		printf("Value of array fron rank 0 \n"); 
-		for(int i = 0; i < 3; i++)
+		printf("Value of array fron rank %i \n", rank); 
+		for(int i = 0; i < N; i++)
 		{
 			printf("%i, ", buf[i]); 
 		}
 		printf("\n"); 
 	}
-	/* Free window and groups */
-	MPI_Win_free(&win);
-	MPI_Group_free(&group);
-	MPI_Group_free(&comm_group);
 
 	//Shut down...
 	MPI_Finalize();
