96e929e8809b7770c59db461632dd1a49c7b2351
Shrey <sb15895@my.bristol.ac.uk>
2023-06-05 22:07:57 +0100

    updated c file to create shared memory window

--- oneArray.c
+++ oneArray.c
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
 	MPI_Init(&argc, &argv);
 	MPI_Status status; 
 	int ierr; 
-	
+
 	// MPI related initialisations 
 	int globalRank, globalSize, colour; 
 	MPI_Comm_rank(MPI_COMM_WORLD,&globalRank); 
@@ -35,7 +35,8 @@ int main(int argc, char** argv)
 
 	// allocate memory
 	int soi = sizeof(int);
-	int* array = (int *)malloc(soi*N); 
+	int* array; 
+	// array = (int *)malloc(soi*N); 
 	// int array[N]; 
 
 	/*
@@ -47,11 +48,13 @@ int main(int argc, char** argv)
 	 * Communicator 
 	 */  
 	colour = globalRank%(globalSize/2); // IO rank and comp rank have same colour
-	MPI_Comm_split(MPI_COMM_WORLD, colour, globalRank, &newComm); 
+	ierr = MPI_Comm_split(MPI_COMM_WORLD, colour, globalRank, &newComm); 
+	error_check(ierr); 
 	// newComm = MPI_COMM_WORLD; 
 	// test
 	int newRank; 
-	MPI_Comm_rank(newComm,&newRank); 
+	ierr = MPI_Comm_rank(newComm,&newRank); 
+	error_check(ierr); 
 	printf("Global rank=%i, New rank=%i, Colour=%i \n", globalRank, newRank, colour); 
 	MPI_Win win; 
 
@@ -60,53 +63,73 @@ int main(int argc, char** argv)
 	start = MPI_Wtime();
 
 	// comp process initialises array and creates a window with that array 
+
 	if(newRank == 0)  
 	{
-		initialise(array, 5); 
-		printf("memory initialised by globalrank %i and newRank %i \n",globalRank, newRank); 
-		ierr = MPI_Win_create(array, soi*N, soi, MPI_INFO_NULL, newComm, &win);
+		ierr = MPI_Win_allocate_shared(soi*N, soi, MPI_INFO_NULL, newComm, &array, &win); 
 		error_check(ierr); 
-	} 
+		printf("memory initialised by globalrank %i and newRank %i \n",globalRank, newRank); 
+		//ierr = MPI_Win_create(array, soi*N, soi, MPI_INFO_NULL, newComm, &win);
 
-	else
+		// create lock and initialise arrays 
+		MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 0, MPI_MODE_NOCHECK, win);
+		initialise(array,5); 
+		MPI_Win_unlock(0,win);
+	} 
+	else 
 	{
 		// non 0 rank creates window using NULL pointer 
-		printf("null window created by globalrank %i and newRank %i \n",globalRank, newRank); 
-		ierr = MPI_Win_create(NULL, 0,1, MPI_INFO_NULL, newComm, &win);
+		// printf("null window created by globalrank %i and newRank %i \n",globalRank, newRank); 
+		// ierr = MPI_Win_create(NULL, 0,soi, MPI_INFO_NULL, newComm, &win);
+		ierr = MPI_Win_allocate_shared(0, soi, MPI_INFO_NULL, newComm, &array, &win); 
 		error_check(ierr); 
+		long int arraySize; 
+		int dispUnit; 
+		ierr = MPI_Win_shared_query(win, 0, &arraySize, &dispUnit, &array); 
+		error_check(ierr); 
+		printf("MPI shared query globalrank %i and newRank %i \n",globalRank, newRank); 
 	} 
-	
+
+	MPI_Barrier(newComm); 
+
+	if(newRank!=0)
+	{
+		printData(array); 
+	} 
+
+	MPI_Barrier(newComm); 
+
 	// start window 
 	printf("MPI window fence created globalrank %i and newRank %i \n",globalRank, newRank); 
-	MPI_Win_fence(MPI_MODE_NOPRECEDE,win);
+	//ierr = MPI_Win_fence(MPI_MODE_NOPRECEDE,win);
+	//error_check(ierr); 
 
-	if(newRank != 0)
-	{
-		MPI_Get(array, N, MPI_INT, 0,0,N, MPI_INT, win); 
-		printf("MPI get using globalrank %i and newRank %i \n",globalRank, newRank); 
-	} 
-
-	MPI_Win_fence(MPI_MODE_NOSUCCEED, win);
-	MPI_Win_free(&win);
+	//ierr = MPI_Win_fence(MPI_MODE_NOSUCCEED, win);
+	//error_check(ierr); 
+	ierr = MPI_Win_free(&win);
+	error_check(ierr); 
 	printf("MPI window freed by globalrank %i and newRank %i \n",globalRank, newRank); 
 	// end window
-	
+
 	// print values 
-	//if(newRank != 0)
-	//{
-	//	printf("Global rank %i reading array with value set to be 5 \n",globalRank); 
-	//	printData(array); 
-	//} 
+	if(newRank != 0)
+	{
+		printf("Global rank %i reading array with value set to be 5 \n",globalRank); 
+		// printData(array); 
+	} 
 
 	stop = MPI_Wtime();
 	diff = stop - start;
 
 	printf("Rank %d, took %8.8fs\n",
 			globalRank,  diff);
+	
+	// free(array); 
+	// array = NULL; 
+	// printf("array freed \n"); 
 
 	MPI_Finalize();
-	free(array); 
-	array = NULL; 
+	printf("MPI finalise \n"); 
 	return 0;
 } 
 
