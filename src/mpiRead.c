d7fa921038f4798ede17cfe36ad132cac89b7f47
Shrey <sb15895@my.bristol.ac.uk>
2023-08-30 09:57:46 +0100

    mpiread debug comments now write to file

--- src/mpiRead.c
+++ src/mpiRead.c
@@ -40,30 +40,30 @@ void mpiRead(double *readData, char* FILENAME, struct params *ioParams)
 	error_check(ierr); 
 
 #ifndef NDEBUG   
-	printf("Cartcomm rank and size %i %i \n", myrank, nprocs); 
-	printf("arraygsize for rank %i : %i  \n",myrank, globalArray[0]	); 
-	printf("arraysubsize for rank %i : %i \n",myrank,localArray[0]); 
-	printf("arraystart for rank %i : %i  \n",myrank,arrayStart[0]		); 
+	fprintf(ioParams->debug,"Cartcomm rank and size %i %i \n", myrank, nprocs); 
+	fprintf(ioParams->debug,"arraygsize for rank %i : %i  \n",myrank, globalArray[0]	); 
+	fprintf(ioParams->debug,"arraysubsize for rank %i : %i \n",myrank,localArray[0]); 
+	fprintf(ioParams->debug,"arraystart for rank %i : %i  \n",myrank,arrayStart[0]		); 
 #endif 
 
 	ierr = MPI_Type_create_subarray(NDIM, globalArray, localArray, arrayStart,
 			MPI_ORDER_C, MPI_DOUBLE, &filetype); 
 	error_check(ierr); 
 #ifndef NDEBUG   
-	printf("MPI create subarray \n"); 
+	fprintf(ioParams->debug,"MPI create subarray \n"); 
 #endif                                   
 
 	ierr = MPI_Type_commit(&filetype); 
 	error_check(ierr); 
 #ifndef NDEBUG   
-	printf("MPI type commit \n"); 
+	fprintf(ioParams->debug,"MPI type commit \n"); 
 #endif       
 
 	ierr = MPI_File_open(ioParams->cartcomm, FILENAME, MPI_MODE_RDONLY, 
 			MPI_INFO_NULL, &fh); 
 	error_check(ierr); 
 #ifndef NDEBUG   
-	printf("MPI file open \n"); 
+	fprintf(ioParams->debug,"MPI file open \n"); 
 #endif       
 
 	// Set view for this process using datatype 
@@ -71,7 +71,7 @@ void mpiRead(double *readData, char* FILENAME, struct params *ioParams)
 			MPI_INFO_NULL); 
 	error_check(ierr); 
 #ifndef NDEBUG   
-	printf("MPI file set view \n"); 
+	fprintf(ioParams->debug,"MPI file set view \n"); 
 #endif       
 
 	// Remove halo data    
@@ -83,19 +83,19 @@ void mpiRead(double *readData, char* FILENAME, struct params *ioParams)
 	ierr = MPI_File_read_all(fh, readData, total_data, MPI_DOUBLE, &status);   
 	error_check(ierr); 
 #ifndef NDEBUG   
-	printf("MPI file write all \n"); 
+	fprintf(ioParams->debug,"MPI file write all \n"); 
 #endif       
 
 	ierr = MPI_File_close(&fh);
 	error_check(ierr); 
 #ifndef NDEBUG   
-	printf("MPI file close \n"); 
+	fprintf(ioParams->debug,"MPI file close \n"); 
 #endif       
 
 	ierr = MPI_Type_free(&filetype); 
 	error_check(ierr); 
 #ifndef NDEBUG   
-	printf("MPI filetype\n"); 
+	fprintf(ioParams->debug,"MPI filetype\n"); 
 #endif       
 
 }
